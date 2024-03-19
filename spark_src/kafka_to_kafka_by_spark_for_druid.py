dfa95ecd6b272e958acd664cc1d0818436f731b6
7xxogre <bestech82@naver.com>
2024-03-19 14:56:23 +0900

    kafka로 스트리밍 데이터을 받고, 이를 다시 kafka로 보내는 코드 작성. Apache Druid에서 이를 받을 예정 및 내부 처리 코드는 이후 추가 예정

--- spark_src/kafka_to_kafka_by_spark_for_druid.py
+++ spark_src/kafka_to_kafka_by_spark_for_druid.py
@@ -1,15 +1,16 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import expr, from_json, col, current_date
+from pyspark.sql.functions import from_json, col, current_date
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType, DoubleType, ArrayType
 
 import threading
-# 아래 명령어처럼 kafka를 추가해 스파크를 실행해 주어야함.
+
 # spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2 kafka_spark_consumer.py
 kafka_bootstrap_servers = "kafka_brokder1:9092,kafka_brokder2:9092,kafka_brokder3:9092"
 upbit_orderbook_topic = "upbit_orderbook"
 upbit_trade_topic = "upbit_trade"
 
-spark = SparkSession.builder.appName("kafka-consume").getOrCreate()
+spark = SparkSession.builder.appName("kafka-to-kafka-streaming").getOrCreate()
+
 orderbook_df = spark.readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
@@ -20,7 +21,6 @@ trade_df = spark.readStream \
         .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
         .option("subscribe", upbit_trade_topic) \
         .load()
-
 
 tradeSchema = StructType([
     StructField("type", StringType(), True),
@@ -57,42 +57,38 @@ orderbookSchema = StructType([
 ])
 
 transformed_orderbook_df = orderbook_df.selectExpr("CAST(value AS STRING)") \
-                    .select(from_json(col("value"), orderbookSchema).alias("data")) \
-                    .select("data.type", "data.code", "data.timestamp", "data.total_ask_size", "data.total_bid_size", "data.orderbook_units")
-
+                                        .select(from_json(col("value"), orderbookSchema).alias("data")) \
+                                        .select("data.type", "data.code", "data.timestamp", "data.total_ask_size", "data.total_bid_size", "data.orderbook_units")
 transformed_trade_df = trade_df.selectExpr("CAST(value AS STRING)") \
-                    .select(from_json(col("value"), tradeSchema).alias("data")) \
-                    .select("data.type", "data.code", "data.timestamp", "data.trade_timestamp", "data.trade_price", "data.trade_volume", "data.ask_bid")
+                                .select(from_json(col("value"), orderbookSchema).alias("data")) \
+                                .select("data.type", "data.code", "data.timestamp", "data.trade_timestamp", "data.trade_price", "data.trade_volume", "data.ask_bid")
 
 date_orderbook_df = transformed_orderbook_df.withColumn("processing_date", current_date())
 date_trade_df = transformed_trade_df.withColumn("processing_date", current_date())
 
-# checkpoint는 다르게 해야해!!! 같이 하면 나중꺼밖에 저장이 안됨
-# gcs 이름 설정해주기!!!
-query1 = date_orderbook_df.writeStream \
-                    .format("json") \
-                    .option("checkpointLocation", "gs://gcs-name/checkpoints/upbit/orderbook/") \
-                    .option("path", "gs://gcs-name/raw-data/upbit/orderbook/") \
-                    .partitionBy("processing_date", "code") \
-                    .trigger(processingTime='30 minutes') \
-                    .start()
+ob_query = date_orderbook_df.writeStream \
+                        .format("kafka") \
+                        .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+                        .option("topic", "processed_upbit_orderbook") \
+                        .option("checkpointLocation", "/path/to/checkpoint/upbit_ob") \
+                        .trigger(processingTime = "2 seconds") \
+                        .start()
 
-query2 = date_trade_df.writeStream \
-                    .format("json") \
-                    .option("checkpointLocation", "gs://gcs-name/checkpoints/upbit/trade/") \
-                    .option("path", "gs://gcs-name/raw-data/upbit/trade/") \
-                    .partitionBy("processing_date", "code") \
-                    .trigger(processingTime='30 minutes') \
-                    .start()
+tr_query = date_trade_df.writeStream \
+                        .format("kafka") \
+                        .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+                        .option("topic", "processed_upbit_orderbook") \
+                        .option("checkpointLocation", "/path/to/checkpoint/upbit_tr") \
+                        .trigger(processingTime = "2 seconds") \
+                        .start()
 
 
-# query1.awaitTermination()
-# query2.awaitTermination()
+
 def awaitTermination(query):
     query.awaitTermination()
 
-thread1 = threading.Thread(target = awaitTermination, args=(query1,))
-thread2 = threading.Thread(target = awaitTermination, args=(query2,))
+thread1 = threading.Thread(target = awaitTermination, args=(ob_query,))
+thread2 = threading.Thread(target = awaitTermination, args=(tr_query,))
 
 thread1.start()
 thread2.start()
