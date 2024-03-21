b93fb3780d5a22074870830c2ed26a99aa37573d
7xxogre <bestech82@naver.com>
2024-03-21 18:29:02 +0900

    dataproc & airflow에 맞게 변경

--- spark_src/kafka_to_gcs_by_spark_batch.py
+++ spark_src/kafka_to_gcs_by_spark_batch.py
@@ -4,7 +4,6 @@ import pickle
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, current_date
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType, DoubleType, ArrayType
-from kafka import KafkaConsumer, TopicPartition
 from datetime import timedelta
 from google.cloud import storage
 
@@ -18,25 +17,6 @@ def download_blob(bucket_name, source_blob_name):
     contents = blob.download_as_string()
     
     return contents.decode("utf-8").split()
-
-def get_kafka_offset(bootstrap_servers, topic_name, num_partition, execution_date, interval_hours):
-    consumer = KafkaConsumer(
-        bootstrap_servers=bootstrap_servers,
-        auto_offset_reset='earliest',
-        consumer_timeout_ms=1000
-    )
-
-    prev_execution_date = execution_date - timedelta(hours = interval_hours)
-    topic_partitions = TopicPartition(topic_name, num_partition)
-
-    start_offsets = consumer.offsets_for_times({
-        tp: int(prev_execution_date.timestamp() * 1000) for tp in topic_partitions
-    })
-    end_offsets = consumer.offsets_for_times({
-        tp: int(execution_date.timestamp() * 1000) for tp in topic_partitions
-    })
-    return {"start_offset": start_offsets[topic_partitions[0]].offset, 
-            "end_offset": end_offsets[topic_partitions[0]].offset}
 
 def load_schema(topic_name: str) -> StructType:
     if "upbit_trade" == topic_name:
@@ -81,37 +61,40 @@ parser = argparse.ArgumentParser(description='Spark job arguments')
 parser.add_argument('--kafka-bootstrap-server-list-file-name', required=True, type=str, help='Kafka broker ip list file')
 parser.add_argument('--topic-name', required=True, type=str, help='Kafka topic name')
 parser.add_argument('--num-partitions', required=True, type=int, help='Number of partitions')
-parser.add_argument('--execution_date', required=True, type=str, help='Airflow task execution date')
+parser.add_argument('--execution-date', required=True, type=str, help='Airflow task execution date')
 parser.add_argument('--gcs-name', required=True, type=str, help='Google Cloud Storage name')
 parser.add_argument('--gcs-save-path', required=True, type=str, help='Google Cloud Storage save path')
 parser.add_argument('--app-name', required=True, type=str, help='Spark app name')
+parser.add_argument('--kafka-start-offset', required=True, type=str, help='kafka start offset')
+parser.add_argument('--kafka-end-offset', required=True, type=str, help='kafka end offset')
 args = parser.parse_args()
 
 kafka_bootstrap_servers = download_blob(args.gcs_name, args.kafka_bootstrap_server_list_file_name)
 kafka_bootstrap_servers_str = ",".join(kafka_bootstrap_servers)
-offsets = get_kafka_offset(kafka_bootstrap_servers_str, args.topic_name, args.num_partition, )
-start_offset, end_offset = offsets['start_offset'], offsets['end_offset']
+
+start_offset, end_offset = args.kafka_start_offset, args.kafka_end_offset
 
 if start_offset < end_offset:
-    spark = SparkSession.builder.appName(args.app_name).getOrCreate()
+    spark = SparkSession.builder.appName(args.app_name) \
+                        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2,org.apache.kafka:kafka-clients:3.2.1") \
+                        .getOrCreate()
     # schema load
     schema = load_schema(args.topic_name)
     df = spark.read \
             .format("kafka") \
             .option("kafka.bootstrap.servers", kafka_bootstrap_servers_str) \
             .option("subscribe", args.topic_name) \
-            .option("startingOffsets", f"""{{"{args.topic_name}":{{"{args.num_partition}":{start_offset}}}}}""") \
-            .option("endingOffsets", f"""{{"{args.topic_name}":{{"{args.num_partition}":{end_offset}}}}}""") \
+            .option("startingOffsets", f"""{{"{args.topic_name}":{{"{args.num_partitions}":{start_offset}}}}}""") \
+            .option("endingOffsets", f"""{{"{args.topic_name}":{{"{args.num_partitions}":{end_offset}}}}}""") \
             .load()
 
     transformed_df = df.selectExpr("CAST(value AS STRING)") \
                         .select(from_json(col("value"), schema).alias("data"))
-    date_df = transformed_df.withColumn("start_offset", start_offset).withColumn("end_offset", end_offset).withColumn("processing_date", current_date())
+    date_df = transformed_df.withColumn("processing_date", current_date()).withColumn("code", col("data.code"))
 
     date_df.write \
         .format("json") \
-        .option("checkpointLocation", f"gs://{args.gcs_name}/checkpoints/{args.gcs_save_path}/") \
         .option("path", f"gs://{args.gcs_name}/raw-data/{args.gcs_save_path}/") \
-        .partitionBy("processing_date", "code") \
+        .partitionBy("processing_date", "code").mode("append") \
         .save()
     spark.stop()
