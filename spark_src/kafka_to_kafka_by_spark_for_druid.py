a57f3e732768f22abefc487e19b48f2afe7d4dbf
7xxogre <bestech82@naver.com>
2024-03-29 02:38:40 +0900

    코드 유지보수를 위한 코딩 스타일 변경

--- spark_src/kafka_to_kafka_by_spark_for_druid.py
+++ spark_src/kafka_to_kafka_by_spark_for_druid.py
@@ -21,34 +21,16 @@ upbit_trade_topic = "upbit_trade"
 
 spark = SparkSession.builder.appName("kafka-to-kafka-streaming").getOrCreate()
 
-orderbook_df = spark.readStream \
+ob_df = spark.readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
         .option("subscribe", upbit_orderbook_topic) \
         .load()
-trade_df = spark.readStream \
+tr_df = spark.readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
         .option("subscribe", upbit_trade_topic) \
         .load()
-
-tradeSchema = StructType([
-    StructField("type", StringType(), True),
-    StructField("code", StringType(), True),
-    StructField("timestamp", LongType(), True),
-    StructField("trade_date", StringType(), True),
-    StructField("trade_time", StringType(), True),
-    StructField("trade_timestamp", LongType(), True),
-    StructField("trade_price", DoubleType(), True),
-    StructField("trade_volume", DoubleType(), True),
-    StructField("ask_bid", StringType(), True),
-    StructField("prev_closing_price", DoubleType(), True),
-    StructField("change", StringType(), True),
-    StructField("change_price", DoubleType(), True),
-    StructField("sequential_id", LongType(), True),
-    StructField("stream_type", StringType(), True),
-    StructField("arrive_time", DoubleType(), True)
-])
 
 orderbookUnitSchema = StructType([
     StructField("ask_price", DoubleType(), True),
@@ -68,64 +50,108 @@ orderbookSchema = StructType([
     StructField("arrive_time", DoubleType(), True)
 ])
 
-transformed_orderbook_df = orderbook_df.selectExpr("CAST(value AS STRING)") \
-                                        .select(from_json(col("value"), orderbookSchema).alias("data")) \
-                                        .select("data.code", "data.timestamp", "data.total_ask_size", "data.total_bid_size", "data.orderbook_units", "data.arrive_time")
-transformed_trade_df = trade_df.selectExpr("CAST(value AS STRING)") \
-                                .select(from_json(col("value"), tradeSchema).alias("data")) \
-                                .select("data.code", "data.timestamp", "data.trade_timestamp", "data.trade_price", "data.trade_volume", "data.ask_bid", "data.arrive_time")
-
-date_orderbook_df = transformed_orderbook_df.withColumn("time_diff", col("arrive_time") - col("timestamp") / 1000) \
-                                            .withColumn("server_datetime", to_timestamp(from_unixtime(col("timestamp") / 1000)))
-date_trade_df = transformed_trade_df.withColumn("time_diff", col("arrive_time") - col("timestamp") / 1000) \
-                                    .withColumn("server_datetime", to_timestamp(from_unixtime(col("timestamp") / 1000)))
-
-processed_ob_df = date_orderbook_df.withWatermark("server_datetime", "10 second") \
-                                    .groupBy(window(col("server_datetime"), "10 second"), "code").agg(
-                                        func.expr("last(orderbook_units[0].ask_price) as ask_price"),
-                                        func.expr("last(orderbook_units[0].bid_price) as bid_price"),
-                                        func.last(col("server_datetime")).alias("server_datetime"),
-                                        func.last(col("timestamp")).alias("server_time"),
-                                        func.last(col("arrive_time")).alias("arrive_time"),
-                                        func.mean(col("time_diff")).alias("time_diff")
-                                    )
-processed_tr_df = date_trade_df.withWatermark("server_datetime", "10 second") \
-                                .groupBy(window(col("server_datetime"), "10 second"), "code").agg(
-                                    func.first(col("trade_price")).alias("open"),
-                                    func.max(col("trade_price")).alias("high"),
-                                    func.min(col("trade_price")).alias("low"),
-                                    func.last(col("trade_price")).alias("close"),
-                                    func.last(col("server_datetime")).alias("server_datetime"),
-                                    func.last(col("timestamp")).alias("server_time"),
-                                    func.last(col("trade_timestamp")).alias("trade_time"),
-                                    func.sum(col("trade_volume")).alias("total_trade_volume"),
-                                    func.sum(func.when(col("ask_bid") == "ASK", col("trade_volume")).otherwise(0)).alias("total_ask_volume"),
-                                    func.sum(func.when(col("ask_bid") == "BID", col("trade_volume")).otherwise(0)).alias("total_bid_volume"),
-                                    func.last(col("arrive_time")).alias("arrive_time"),
-                                    func.mean(col("time_diff")).alias("time_diff")
-                                )
+tradeSchema = StructType([
+    StructField("type", StringType(), True),
+    StructField("code", StringType(), True),
+    StructField("timestamp", LongType(), True),
+    StructField("trade_date", StringType(), True),
+    StructField("trade_time", StringType(), True),
+    StructField("trade_timestamp", LongType(), True),
+    StructField("trade_price", DoubleType(), True),
+    StructField("trade_volume", DoubleType(), True),
+    StructField("ask_bid", StringType(), True),
+    StructField("prev_closing_price", DoubleType(), True),
+    StructField("change", StringType(), True),
+    StructField("change_price", DoubleType(), True),
+    StructField("sequential_id", LongType(), True),
+    StructField("stream_type", StringType(), True),
+    StructField("arrive_time", DoubleType(), True)
+])
 
 
-json_ob_df = processed_ob_df.select(func.to_json(func.struct(*processed_ob_df.columns)) \
-                                    .alias('value'))
-json_tr_df = processed_tr_df.select(func.to_json(func.struct(*processed_tr_df.columns)) \
-                                    .alias('value'))
+transformed_ob_df = ob_df.selectExpr("CAST(value AS STRING)") \
+                        .select(from_json(col("value"), orderbookSchema).alias("data")) \
+                        .select("data.code", "data.timestamp", 
+                                "data.total_ask_size", "data.total_bid_size", 
+                                "data.orderbook_units", "data.arrive_time")
 
+transformed_tr_df = tr_df.selectExpr("CAST(value AS STRING)") \
+                        .select(from_json(col("value"), tradeSchema).alias("data")) \
+                        .select("data.code", "data.timestamp", "data.trade_timestamp", 
+                                "data.trade_price", "data.trade_volume", 
+                                "data.ask_bid", "data.arrive_time")
+
+date_ob_df = transformed_ob_df.withColumn("time_diff", 
+                                          col("arrive_time") - col("timestamp") / 1000) \
+                        .withColumn("server_datetime", 
+                                    to_timestamp(from_unixtime(col("timestamp") / 1000)))
+date_tr_df = transformed_tr_df.withColumn("time_diff", 
+                                          col("arrive_time") - col("timestamp") / 1000) \
+                        .withColumn("server_datetime",
+                                     to_timestamp(from_unixtime(col("timestamp") / 1000)))
+
+processed_ob_df = date_ob_df.withWatermark("server_datetime", "10 second") \
+                .groupBy(window(col("server_datetime"), "10 second"), "code") \
+                .agg(
+                    func.expr("last(orderbook_units[0].ask_price) as ask_price"),
+                    func.expr("last(orderbook_units[0].bid_price) as bid_price"),
+                    func.last(col("server_datetime")).alias("server_datetime"),
+                    func.last(col("timestamp")).alias("server_time"),
+                    func.last(col("arrive_time")).alias("arrive_time"),
+                    func.mean(col("time_diff")).alias("time_diff")
+                )
+processed_tr_df = date_tr_df.withWatermark("server_datetime", "10 second") \
+                .groupBy(window(col("server_datetime"), "10 second"), "code") \
+                .agg(
+                    func.first(col("trade_price")).alias("open"),
+                    func.max(col("trade_price")).alias("high"),
+                    func.min(col("trade_price")).alias("low"),
+                    func.last(col("trade_price")).alias("close"),
+                    func.last(col("server_datetime")).alias("server_datetime"),
+                    func.last(col("timestamp")).alias("server_time"),
+                    func.last(col("trade_timestamp")).alias("trade_time"),
+                    func.sum(col("trade_volume")).alias("total_trade_volume"),
+                    func.sum(
+                        func.when(
+                            col("ask_bid") == "ASK", col("trade_volume")
+                        ).otherwise(0)
+                    ).alias("total_ask_volume"),
+                    func.sum(
+                        func.when(
+                            col("ask_bid") == "BID", col("trade_volume")
+                        ).otherwise(0)
+                    ).alias("total_bid_volume"),
+                    func.last(col("arrive_time")).alias("arrive_time"),
+                    func.mean(col("time_diff")).alias("time_diff")
+                )
+
+
+json_ob_df = processed_ob_df.select(
+                    func.to_json(func.struct(*processed_ob_df.columns)
+                 ).alias('value'))
+json_tr_df = processed_tr_df.select(
+                    func.to_json(func.struct(*processed_tr_df.columns)
+                 ).alias('value'))
+
+ob_processed_topic = "processed_upbit_orderbook"
+ob_chackpoint_loc = "/path/to/checkpoint/upbit_ob_with_arrive_time"
 ob_query = json_ob_df.writeStream \
-                        .format("kafka") \
-                        .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
-                        .option("topic", "processed_upbit_orderbook") \
-                        .option("checkpointLocation", "/path/to/checkpoint/upbit_ob_with_arrive_time") \
-                        .trigger(processingTime = "10 seconds") \
-                        .start()
+                .format("kafka") \
+                .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+                .option("topic", ob_processed_topic) \
+                .option("checkpointLocation", ob_chackpoint_loc) \
+                .trigger(processingTime = "10 seconds") \
+                .start()
 
+tr_processed_topic = "processed_upbit_trade"
+tr_chackpoint_loc = "/path/to/checkpoint/upbit_tr_with_arrive_time"
 tr_query = json_tr_df.writeStream \
-                        .format("kafka") \
-                        .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
-                        .option("topic", "processed_upbit_trade") \
-                        .option("checkpointLocation", "/path/to/checkpoint/upbit_tr_with_arrive_time") \
-                        .trigger(processingTime = "10 seconds") \
-                        .start()
+                .format("kafka") \
+                .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+                .option("topic", tr_processed_topic) \
+                .option("checkpointLocation", tr_chackpoint_loc) \
+                .trigger(processingTime = "10 seconds") \
+                .start()
 
 
 
