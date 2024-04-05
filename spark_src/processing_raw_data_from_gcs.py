af9aa38a8019d06bc93672fbf4fe003f01ba6650
7xxogre <bestech82@naver.com>
2024-04-05 12:01:52 +0900

    gcs 데이터 로드 코드 함수로 통일 및 함수들에 주석 추가

--- spark_src/processing_raw_data_from_gcs.py
+++ spark_src/processing_raw_data_from_gcs.py
@@ -1,18 +1,26 @@
 import argparse
-import json
-import pickle
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as func
-from pyspark.sql.functions import from_json, col, current_date, Window, last, \
-                                    col, first, last, when, floor
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType, DoubleType, ArrayType
-from datetime import timedelta
-from google.cloud import storage
+from pyspark.sql.functions import col, Window, col
+from pyspark.sql.types import StructType, StructField, StringType, LongType, \
+                                IntegerType, DoubleType, ArrayType
+"""
+    GCS의 RAW orderbook과 trade 데이터를 전처리해 GCS에 저장하는 코드
+    Airflow의 DAG를 사용하여 스케쥴링
+"""
+def load_schema(schema_name: str) -> StructType:
+    """ topic 이름에 맞는 schema를 리턴
+    
+    parameter:
+        schema_name: 스키마의 이름 (String)
 
-def load_schema(topic_name: str) -> StructType:
-    if topic_name not in ['upbit_trade', 'upbit_orderbook']:
-        raise Exception("topic name이 적절하지 않습니다. [upbit_trade, upbit_orderbook] 중에 있어야합니다.")
-    if "upbit_trade" == topic_name:
+    return:
+        Pyspark Schema: 정의된 스키마 (StructType)
+    """
+    if schema_name not in ['upbit_trade', 'upbit_orderbook']:
+        raise Exception("topic name이 적절하지 않습니다. \
+                        [upbit_trade, upbit_orderbook] 중에 있어야합니다.")
+    if "upbit_trade" == schema_name:
         return StructType([
             StructField("type", StringType(), True),
             StructField("code", StringType(), True),
@@ -31,7 +39,7 @@ def load_schema(topic_name: str) -> StructType:
             StructField("arrive_time", DoubleType(), True)
         ])
     
-    if "upbit_orderbook" == topic_name:
+    if "upbit_orderbook" == schema_name:
         upbitOrderbookUnitSchema = StructType([
             StructField("ask_price", DoubleType(), True),
             StructField("bid_price", DoubleType(), True),
@@ -45,11 +53,35 @@ def load_schema(topic_name: str) -> StructType:
             StructField("timestamp", LongType(), True),
             StructField("total_ask_size", DoubleType(), True),
             StructField("total_bid_size", DoubleType(), True),
-            StructField("orderbook_units", ArrayType(upbitOrderbookUnitSchema), True),
+            StructField("orderbook_units", 
+                        ArrayType(upbitOrderbookUnitSchema), True),
             StructField("stream_type", StringType(), True),
             StructField("level", IntegerType(), True),
             StructField("arrive_time", DoubleType(), True)
         ])
+
+def get_raw_data_df_from_gcs(schema_name: str, args: argparse.Namespace, folder_name: str, spark):
+    """ gcs의 raw 데이터를 spark df로 로드하는 함수
+
+    parameter:
+        schema_name: 스키마 이름
+        args: 제공된 argment parser
+        folder_name: args.gcs_save_path 이하 폴더 이름 (orderbook 또는 trade)
+        spark: 빌드된 SparkSession
+
+    return: pyspark dataframe
+    """
+    if folder_name not in ['orderbook', 'trade']:
+        raise Exception("folder_name이 적절치 않습니다. \
+                        folder_name은 ['orderbook', 'trade'] 내에 속해야 합니다.")
+    
+    schema = load_schema(schema_name)
+
+    gcs_path = f"gs://{args.gcs_name}/raw-data/{args.gcs_save_path}/{folder_name}/processing_date={args.execution_date}/**/*.json"
+
+    df = spark.read.schema(schema).json(gcs_path)
+    return df
+
 
 parser = argparse.ArgumentParser(description='Spark job arguments')
 parser.add_argument('--kafka-bootstrap-server-list-file-name', required=True, type=str, help='Kafka broker ip list file')
@@ -60,29 +92,26 @@ parser.add_argument('--app-name', required=True, type=str, help='Spark app name'
 parser.add_argument('--dollar-bar-size', required=True, type=int, help='sampling dollar bar size')
 args = parser.parse_args()
 
-spark = SparkSession.builder.appName(args.app_name) \
-                .getOrCreate()
-orderbook_schema = load_schema("upbit_orderbook")
-trade_schema = load_schema("upbit_trade")
+spark = SparkSession.builder.appName(args.app_name).getOrCreate()
 
-orderbook_gcs_path = f"gs://{args.gcs_name}/raw-data/{args.gcs_save_path}/orderbook/processing_date={args.execution_date}/**/*.json"
-trade_gcs_path = f"gs://{args.gcs_name}/raw-data/{args.gcs_save_path}/trade/processing_date={args.execution_date}/**/*.json"
-
-orderbook_df = spark.read.schema(orderbook_schema).json(orderbook_gcs_path) \
+orderbook_df = get_raw_data_df_from_gcs("upbit_orderbook", args, "orderbook", spark) \
                     .select("code", "timestamp", "orderbook_units") \
                     .withColumnRenamed("timestamp", "ob_timestamp")
 
-trade_df = spark.read.schema(trade_schema).json(trade_gcs_path) \
-                .select("code", "timestamp", "trade_price", "trade_volume", "ask_bid")
+trade_df = get_raw_data_df_from_gcs("upbit_trade", args, "trade", spark)\
+                    .select("code", "timestamp", "trade_price", 
+                            "trade_volume", "ask_bid")
 
 trade_dollar_df = trade_df.withColumn("trade_dollar",
                                       col("trade_volume") * col("trade_price")) \
                         .orderBy(col("code"), col("timestamp"))
 
 window = Window.partitionBy("code").orderBy("timestamp")
+
 trade_cumsum_df = trade_dollar_df.withColumn("cumsum", 
                                              sum("trade_dollar").over(window)) \
                                 .drop("trade_dollar")
+
 trade_dollar_bar_df = trade_cumsum_df.withColumn("dollar_bar_num",
                                         col("cumsum") // args.dollar_bar_size) \
                                     .drop("cumsum")
@@ -112,7 +141,7 @@ tr_sampled_by_dollar_bar_df = \
 join_condition = [
     orderbook_df.code == tr_sampled_by_dollar_bar_df.code,
     orderbook_df.ob_timestamp <= tr_sampled_by_dollar_bar_df.timestamp,
-    orderbook_df.ob_timestamp >= tr_sampled_by_dollar_bar_df.timestamp - 1000 * 10
+    orderbook_df.ob_timestamp >= tr_sampled_by_dollar_bar_df.timestamp - 1000*10
 ]
 
 tr_ob_joined_df = tr_sampled_by_dollar_bar_df.join(orderbook_df, 
@@ -120,14 +149,15 @@ tr_ob_joined_df = tr_sampled_by_dollar_bar_df.join(orderbook_df,
 
 window_spec = Window.partitionBy("code", "dollar_bar_num") \
                     .orderBy(col("ob_timestamp").desc())
+
 final_joined_df = tr_ob_joined_df.withColumn("row_num", func.row_number().over(window_spec)) \
                                 .filter(col("row_num") == 1) \
                                 .drop("row_num", "ob_timestamp") \
                                 .withColumn("processing_date", args.execute_date)
 
-
 final_joined_df.write \
             .format("json") \
             .option("path", f"gs://{args.gcs_name}/processed_data/{args.gcs_save_path}/") \
             .partitionBy("processing_date", "code").mode("append").save()
+
 spark.stop()
